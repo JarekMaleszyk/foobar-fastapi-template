27ae23eb6fea39a4af3ea625dae0a9a52406d287
Jarosław Maleszyk <88442638+JarekMaleszyk@users.noreply.github.com>
2024-01-06 11:50:52 +0100

    user, role, model init

---
+++ app/models.py
@@ -0,0 +1,34 @@
+from .database import Base
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
+from sqlalchemy.orm import relationship
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    login = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
+class Role(Base):
+    __tablename__ = 'roles'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, nullable=False, unique=True)
+    code = Column(String, nullable=False, unique=True)
+
+class UserRole(Base):
+    __tablename__ = 'user_roles'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), nullable=False)  
+    role_id = Column(Integer, ForeignKey('roles.id', ondelete='CASCADE'), nullable=False)  
+    date_from = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    date_to = Column(TIMESTAMP(timezone=True), nullable=True)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    updated_at = Column(TIMESTAMP(timezone=True), nullable=True)
+
+    user = relationship('User')
+    role = relationship('Role')
