22a700f3c8a1a9971fccce7d0925586b4590b081
Jarek Maleszyk <j.maleszyk@gmail.com>
2024-02-25 15:03:19 +0100

    .

--- app/routers/foo.py
+++ app/routers/foo.py
@@ -20,14 +20,15 @@ def create_foo(foo: schemas.CreateFooDto, db: Session = Depends(get_db)):
     return new_foo
 
 @router.get("/", status_code=status.HTTP_200_OK, response_model=List[schemas.ResponseFooDto])
-def get_many(limit: int = 10,
+def get_many(search: str,
+             limit: int = 10,
              skip: int = 0,
              db: Session = Depends(get_db)):
-    foo = db.query(models.Foo).filter(models.Foo.id == id).first()
-    if not foo:
+    foos = db.query(models.Foo).filter(models.Foo.name.contains(search)).order_by(desc(models.Foo.id)).offset(skip).take(limit).all()
+    if not foos:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f'Foo with id: {id} was not found.')
-    return foo
+                            detail=f'Zero foos were found.')
+    return foos
 
 @router.get("/{id}", status_code=status.HTTP_200_OK, response_model=schemas.ResponseFooDto)
 def get_one(id: int, db: Session = Depends(get_db)):
