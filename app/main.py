8e201a68b42dd30bc8474fcbc439f9cd2c005ed7
Jarek Maleszyk <j.maleszyk@gmail.com>
2024-03-11 18:32:11 +0100

    router.bar

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from .routers import user, role, user_roles, foo
+from .routers import user, role, user_roles, foo, bar
 
 app = FastAPI()
 
@@ -19,6 +19,7 @@ app.include_router(user.router)
 app.include_router(role.router)
 app.include_router(user_roles.router)
 app.include_router(foo.router)
+app.include_router(bar.router)
 
 @app.get("/") 
 def root():
