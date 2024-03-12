58c6a87a5af4c1f3ae65d5e620ea382382599181
Jarosław Maleszyk <jaroslaw.maleszyk@spravia.pl>
2024-03-12 12:13:40 +0100

    sqlite2

--- app/database.py
+++ app/database.py
@@ -3,7 +3,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 #SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
 # engine = create_engine(SQLALCHEMY_DATABASE_URL)
@@ -14,10 +13,14 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 
 # Base = declarative_base()
 
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, 
+                            autoflush=False, 
+                            bind=engine)
 
 Base = declarative_base()
 
