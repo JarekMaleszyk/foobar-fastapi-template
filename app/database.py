630a4d3778c27a2df37c5a308828389d9c3cb579
Jarosław Maleszyk <88442638+JarekMaleszyk@users.noreply.github.com>
2024-03-20 17:44:48 +0100

    .

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = f'mysql+pymysql://root:{settings.database_password}@{settings.database_hostname}:3306/{settings.database_name}'
+SQLALCHEMY_DATABASE_URL = f'postgresql+psycopg2://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
