73d2672efcd151254d67b130122b0eaec7c6563e
Jarosław Maleszyk <88442638+JarekMaleszyk@users.noreply.github.com>
2024-04-08 18:27:56 +0200

    except

--- app/database.py
+++ app/database.py
@@ -2,6 +2,7 @@ from .config import settings
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import logging
 
 SQLALCHEMY_DATABASE_URL = f'postgresql+psycopg2://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
@@ -17,5 +18,7 @@ def get_db():
     db = SessionLocal()
     try:
         yield db
+    except:
+        raise ConnectionError(f'Database connection error, connectionstring: {SQLALCHEMY_DATABASE_URL}')
     finally:
         db.close()
