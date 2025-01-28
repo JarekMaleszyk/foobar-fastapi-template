d808d0515a40baca9850706ceb9eca3a4b9d6890
Jarosław Maleszyk <h4n5pl@gmail.com>
2025-01-28 19:49:35 +0100

    .

--- app/routers/role.py
+++ app/routers/role.py
@@ -13,3 +13,11 @@ router = APIRouter(
 @router.get("/", status_code=status.HTTP_200_OK, response_model=List[schemas.ResponseRoleDto]) 
 def get_all(db: Session = Depends(get_db)):
     return db.query(models.Role).all()
+
+@router.get("/{id}", status_code=status.HTTP_200_OK, response_model=schemas.ResponseRoleDto)
+def get_role(db: Session = Depends(get_db)):
+    role = db.query(models.Role).filter(models.Role.id == id).first()
+    if not role:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Role with id: {id} was not found.')
+    return role
