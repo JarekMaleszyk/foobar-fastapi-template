efedd7c8a4815fdd187df00d6939d8ed1ee4e568
Jarosław Maleszyk <h4n5pl@gmail.com>
2025-03-25 21:20:19 +0100

    docstrings

--- app/routers/user.py
+++ app/routers/user.py
@@ -12,6 +12,19 @@ router = APIRouter(
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.ResponseUserDto)
 def create_user(user: schemas.CreateUserDto, db: Session = Depends(get_db)):
+    """
+    Create a new user in the database.
+
+    Args:
+        user (schemas.CreateUserDto): The user data to create.
+        db (Session): The database session.
+
+    Returns:
+        schemas.ResponseUserDto: The created user data.
+
+    Raises:
+        HTTPException: If a user with the same login or email already exists.
+    """
     user_check = db.query(models.User).filter(models.User.login == user.login or models.User.email == user.email).first()
     if user_check:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
@@ -28,7 +41,22 @@ def create_user(user: schemas.CreateUserDto, db: Session = Depends(get_db)):
 def search_user(db: Session = Depends(get_db),
                 email: Optional[str] = '',
                 limit: int = 10,
-                skip: int = 0,):
+                skip: int = 0):
+    """
+    Search for users in the database by email.
+
+    Args:
+        db (Session): The database session.
+        email (Optional[str]): The email to search for.
+        limit (int): The maximum number of users to return.
+        skip (int): The number of users to skip.
+
+    Returns:
+        List[schemas.ResponseUserDto]: The list of found users.
+
+    Raises:
+        HTTPException: If no users are found.
+    """
     users = db.query(models.User).filter(models.User.email.contains(email)).order_by(desc(models.User.created_at)).offset(skip).limit(limit).all()
     if not users:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -37,6 +65,19 @@ def search_user(db: Session = Depends(get_db),
 
 @router.get("/{id}", status_code=status.HTTP_200_OK, response_model=schemas.ResponseUserDto)
 def get_user(id: int, db: Session = Depends(get_db)):
+    """
+    Get a user by ID.
+
+    Args:
+        id (int): The user ID.
+        db (Session): The database session.
+
+    Returns:
+        schemas.ResponseUserDto: The user data.
+
+    Raises:
+        HTTPException: If the user with the given ID is not found.
+    """
     user = db.query(models.User).filter(models.User.id == id).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
